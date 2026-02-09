2a1b3c1c54fff60c66866fc8e4f33b693fcd1fb9
EdenFood <edenfood.web.site@gmail.com>
2026-02-09 21:27:49 +0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBEPBzcTir3I8meF6c9_NYRDcRJe9j3jW4",
+  authDomain: "eden-36e95.firebaseapp.com",
+  projectId: "eden-36e95",
+  storageBucket: "eden-36e95.firebasestorage.app",
+  messagingSenderId: "52400104172",
+  appId: "1:52400104172:web:9b8ec71d46dd61500cfbc9",
+  measurementId: "G-L6WHRRE7GE"
+};
